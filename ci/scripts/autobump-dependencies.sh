31d378d2a89f1365d88c04881c10f8f1ca219ada
BG <bguttmann@anynines.com>
2025-01-15 13:30:50 +0100

    [WIP] Introduce new CI/CD inspired by HaProxy Bosh Release

--- ci/scripts/autobump-dependencies.sh
+++ ci/scripts/autobump-dependencies.sh
@@ -31,8 +31,11 @@ sed -i -e "s/node_exporter-$USED_NODE_EXPORTER_VERSION\.linux-amd64/node_exporte
 echo $DRY_RUN
 
 if [ -z "$DRY_RUN" ]; then
-  echo "I would have uploaded blobs" 
+  echo "I would have uploaded blobs"
   echo "I would have created a PR"
+else
+  echo "DRY_RUN: bosh upload-blobs --sha2"
+  echo "TODO Create PR"
 fi
 
 rm -rf /tmp/cache/ /tmp/prometheus-blobs/
